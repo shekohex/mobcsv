dad64d5548fe0bb1d6c128bda004665b0a138026
Shady Khalifa <shekohex@gmail.com>
2019-04-03 20:50:09 +0200

    some improvements and fixing bugs
    Signed-off-by: Shady Khalifa <shekohex@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::{
 
 use clap_verbosity_flag::Verbosity;
 use lazy_static::lazy_static;
-use log::info;
+use log::{debug, info};
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use structopt::StructOpt;
@@ -14,11 +14,12 @@ use structopt::StructOpt;
 type CliResult = Result<(), exitfailure::ExitFailure>;
 
 const BUFFER_SIZE: usize = 64 * 1024;
-const MOB_REGEX_STR: &str = "^[0-9]{11,14}$";
+const MOB_REGEX_STR: &str = "^((20)|(966))([0-9]{9,11})$";
 
 lazy_static! {
   static ref MOB_RE: Regex = Regex::new(MOB_REGEX_STR).unwrap();
-  static ref REPLACER_RE: Regex = Regex::new(r#"[!@+#$%\-^&*() ]"#).unwrap();
+  static ref REPLACER_RE: Regex =
+    Regex::new(r#"^(0)|^(00)|[!@+#$%\-^&*() ]"#).unwrap();
 }
 
 #[derive(Debug, StructOpt)]
@@ -60,25 +61,59 @@ fn main() -> CliResult {
   let out = File::create(args.output_path)?;
   let buffer = BufWriter::with_capacity(BUFFER_SIZE, out);
   let mut wrt = csv::Writer::from_writer(buffer);
-  rdr
-    .deserialize()
-    .flatten() // because it returns result !
-    .map(remove_bad_chars)
-    .skip_while(is_bad_number)
-    .try_for_each(|r| wrt.serialize(r))?;
+  for r in rdr.deserialize() {
+    let record = r?;
+    if let Some(d) = is_good_ph(record) {
+      wrt.serialize(d)?;
+    }
+  }
   wrt.flush()?;
   info!("Done !");
   Ok(())
 }
 
+#[inline]
 fn remove_bad_chars(mut record: Record) -> Record {
   // we need to remove all spacial characters to empty one, so we can then
   // validate the mobile number.
-  record.ph = REPLACER_RE.replace_all(&record.ph, "").into();
+  record.ph = REPLACER_RE.replace_all(&record.ph.trim(), "").trim().into();
   record
 }
 
-fn is_bad_number(record: &Record) -> bool { !MOB_RE.is_match(&record.ph) }
+fn standardize_ph(mut record: Record) -> Record {
+  match record.ph.chars().next() {
+    Some('1') => {
+      // Egypt, so we need to add 20
+      record.ph = "20".to_owned() + &record.ph;
+      record
+    },
+    Some('5') => {
+      // Saudi Arabia, add 966
+      record.ph = "966".to_owned() + &record.ph;
+      record
+    },
+    //    Some('0') => {
+    //      // yup it is Egypt, add just 2
+    //      if record.ph.starts_with("01") {
+    //        record.ph = "2".to_owned() + &record.ph;
+    //      }
+    //      record
+    //    },
+    // if none of the above matched then just return it
+    _ => record,
+  }
+}
+
+fn is_good_ph(record: Record) -> Option<Record> {
+  let r = remove_bad_chars(record);
+  let r = standardize_ph(r);
+  if MOB_RE.is_match(&r.ph) {
+    Some(r)
+  } else {
+    debug!("Not Acceptable: {:?}", r);
+    None
+  }
+}
 
 #[cfg(test)]
 mod tests {
@@ -98,26 +133,41 @@ mod tests {
   fn should_detect_bad_numbers() {
     let bad_record = Record::new("20111bad", "test1", 0);
     let bad_record2 = Record::new("hah2011166130", "test2", 0);
-    // short !
     let bad_record3 = Record::new("1232131", "test3", 0);
-    let bad_record4 = Record::new("00", "test3", 0);
-    assert!(is_bad_number(&remove_bad_chars(bad_record)));
-    assert!(is_bad_number(&remove_bad_chars(bad_record2)));
-    assert!(is_bad_number(&remove_bad_chars(bad_record3)));
-    assert!(is_bad_number(&remove_bad_chars(bad_record4)));
+    let bad_record4 = Record::new("00", "test4", 0);
+    let bad_record5 = Record::new("2011166130", "test5", 0);
+    assert!(is_good_ph(bad_record).is_none());
+    assert!(is_good_ph(bad_record2).is_none());
+    assert!(is_good_ph(bad_record3).is_none());
+    assert!(is_good_ph(bad_record4).is_none());
+    assert!(is_good_ph(bad_record5).is_none());
   }
 
   #[test]
   fn should_pass_good_numbers() {
     let good_record = Record::new("201116613061", "test1", 0);
-    let good_record2 = Record::new("+201116613061", "test2", 0);
+    let good_record2 = Record::new("00201116613061", "test2", 0);
     let good_record3 = Record::new("+2(0111)6613061", "test3", 0);
     let good_record4 = Record::new("+2011-1661-3061", "test4", 0);
-    let good_record5 = Record::new("+21116613061", "test5", 0);
-    assert!(!is_bad_number(&remove_bad_chars(good_record)));
-    assert!(!is_bad_number(&remove_bad_chars(good_record2)));
-    assert!(!is_bad_number(&remove_bad_chars(good_record3)));
-    assert!(!is_bad_number(&remove_bad_chars(good_record4)));
-    assert!(!is_bad_number(&remove_bad_chars(good_record5)));
+    let good_record5 = Record::new("+201116613061", "test5", 0);
+    let good_record6 = Record::new("1116613061", "test6", 0);
+    let good_record7 = Record::new("540029129", "test7", 0);
+    let good_record8 = Record::new("5400 291 29", "test8", 0);
+    assert!(is_good_ph(good_record).is_some());
+    assert!(is_good_ph(good_record2).is_some());
+    assert!(is_good_ph(good_record3).is_some());
+    assert!(is_good_ph(good_record4).is_some());
+    assert!(is_good_ph(good_record5).is_some());
+    assert!(is_good_ph(good_record6).is_some());
+    assert!(is_good_ph(good_record7).is_some());
+    assert!(is_good_ph(good_record8).is_some());
+  }
+
+  #[test]
+  fn should_standardize_ph() {
+    let good_record = Record::new("1116613061", "test1", 0);
+    let good_record2 = Record::new("511661306", "test2", 0);
+    assert_eq!(standardize_ph(good_record).ph, "201116613061");
+    assert_eq!(standardize_ph(good_record2).ph, "966511661306");
   }
 }
